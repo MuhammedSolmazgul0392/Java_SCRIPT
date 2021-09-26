68823951381130d464ad832b6b282701066b0cdf
Muhammed Solmazgul <mmsolmazgul039@gmail.com>
2021-09-26 19:00:21 +0200

    ders4_DOM/Beisipiel

---
+++ ders4_dom/ornek1_selector/app.js
@@ -0,0 +1,6 @@
+//==================   GETELEMENTID   ==============================
+
+const paragraf = document.getElementById("selcuk").style;
+paragraf.backgroundColor = "black";
+paragraf.color = "white";
+paragraf.fontSize = "20px";
